136312e6613a6f1b457c7b72c496232fa7a29929
MiguelRosas11 <roguemijose@gmail.com>
2026-04-20 23:42:27 -0600

    Server funcionando correctamente

--- servidor-malo.js
+++ servidor-malo.js
@@ -12,23 +12,25 @@ const server = http.createServer(async (req, res) => {
   }
 
   if (req.url === "/info") {
-    res.writeHead(200, { "Content-Type": "application-json" })
+    res.writeHead(200, { "Content-Type": "application/json" })
     res.end("Ruta de información")
     return
   }
 
   if (req.url === "/api/student") {
     const filePath = path.join(process.cwd(), "datos.json")
-    const texto = fs.readFile(filePath, "utf-8")
+    const texto = await fs.readFile(filePath, "utf-8")
+    const data = JSON.parse(texto)
+
     res.writeHead(200, { "Content-Type": "application/json" })
-    res.end(JSON.stringify(texto))
+    res.end(JSON.stringify(data))
     return
   }
 
-  res.writeHead(200, { "Content-Type": "text/plain" })
+  res.writeHead(404, { "Content-Type": "text/plain" })
   res.end("Ruta no encontrada")
-}
+})
 
 server.listen(PORT, () => {
   console.log("Servidor corriendo en http://localhost:3000")
-}
+})
